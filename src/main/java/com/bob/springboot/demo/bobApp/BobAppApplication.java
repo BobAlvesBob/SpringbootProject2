09eef62d644218fa8570a907c0d1b8ba44e4ec88
BobAlvesBob <bobpitaia@gmail.com>
2024-08-12 21:09:07 -0300

    Initial commit

---
+++ src/main/java/com/bob/springboot/demo/bobApp/BobAppApplication.java
@@ -0,0 +1,13 @@
+package com.bob.springboot.demo.bobApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BobAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BobAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bob/springboot/demo/bobApp/BobAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bob.springboot.demo.bobApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BobAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
